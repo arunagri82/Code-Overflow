5c8ff448e95a02b0ee6748be3977a2d4514c63a9
Lucas Ellery <55142445+lucasellery@users.noreply.github.com>
2020-10-31 23:37:04 -0300

    Create largest-number.js

---
+++ largest-number.js
@@ -0,0 +1,10 @@
+let array = [3, 5, 20, 55, 49, 2, 2, 10];
+let largest= 0;
+
+for(i = 0; i <= largest; i++){
+    if (array[i] > largest) {
+        var largest=array[i];
+    }
+}
+
+console.log(largest);
